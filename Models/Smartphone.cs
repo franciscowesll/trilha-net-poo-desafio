7a577c39e8177e261b55ade29698d001595f8ee2
Francisco <franciscowesll@gmail.com>
2022-11-12 00:11:50 -0300

    terminado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,17 +3,30 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            
         }
+        
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
+            Console.WriteLine("");
+             Console.WriteLine($"Dados do seu celular:");
+              Console.WriteLine($"numero: {this.Numero}");
+              Console.WriteLine($"modelo: {this.Modelo}");
+              Console.WriteLine($"imei: {this.IMEI}");
+              Console.WriteLine($"memoria: {this.Memoria}gb");
+      
         }
 
         public void ReceberLigacao()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Smartphone iphone= new Iphone(numero: "99991253460", modelo:"7", imei:"55-B", memoria:64);
+        Smartphone nokia = new Nokia(numero:"9991884466", modelo:"AS", imei:"7784-C", memoria:128);
+        
+        Console.WriteLine("Smartphone Iphone:");
+        iphone.Ligar();
+        
+        Console.WriteLine("");
+        
+          
+        iphone.InstalarAplicativo("Anki");
+        
+        Console.WriteLine("");
+        Console.WriteLine("");
+        
+        Console.WriteLine("Smartphone Nokia:");   
+        nokia.Ligar();
+        Console.WriteLine("");
+        nokia.InstalarAplicativo("Telegram");
+    }
+}
